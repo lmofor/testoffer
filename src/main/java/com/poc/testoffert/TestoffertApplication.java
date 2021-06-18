cbff7929e2932d21520d360ac402644e351cb796
MOFOR <leon-didier.moffor@atos.net>
2021-06-18 19:52:39 +0000

    first commit

---
+++ src/main/java/com/poc/testoffert/TestoffertApplication.java
@@ -0,0 +1,13 @@
+package com.poc.testoffert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoffertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoffertApplication.class, args);
+	}
+
+}
